5c408e0bc2f4c89c5944706c4e497653f550498c
Sergey Kovtun <sergey.v.kovtun@gmail.com>
2020-11-22 11:28:32 +0200

    init commit

---
+++ src/main/java/com/stones/resttree/RestTreeApplication.java
@@ -0,0 +1,13 @@
+package com.stones.resttree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestTreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stones/resttree/RestTreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stones.resttree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestTreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
